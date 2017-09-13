fb46c97a7453367c40f196194cdb97885d897038
Igor Dr0gis <dr0gis@mail.ru>
2017-09-13 19:47:34 +0300

    Repace birthday EditText on NumberPicker

--- app/src/main/java/com/example/dr0gi/task1users/EditActivity.java
+++ app/src/main/java/com/example/dr0gi/task1users/EditActivity.java
@@ -6,15 +6,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class EditActivity extends AppCompatActivity {
 
+    private EditText editTextName;
+    private EditText editTextSurname;
+    private NumberPicker numberPickerBirthdayMonth;
+    private NumberPicker numberPickerBirthdayDay;
+    private NumberPicker numberPickerBirthdayYear;
     private User user;
 
     @Override
@@ -40,16 +47,28 @@ public class EditActivity extends AppCompatActivity {
                 break;
         }
 
-        EditText editTextName = (EditText) findViewById(R.id.editTextName);
-        EditText editTextSurname = (EditText) findViewById(R.id.editTextSurname);
-        EditText editTextBirthday = (EditText) findViewById(R.id.editTextBirthday);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextSurname = (EditText) findViewById(R.id.editTextSurname);
+        numberPickerBirthdayMonth = (NumberPicker) findViewById(R.id.numberPickerBirthdayMonth);
+        numberPickerBirthdayDay = (NumberPicker) findViewById(R.id.numberPickerBirthdayDay);
+        numberPickerBirthdayYear = (NumberPicker) findViewById(R.id.numberPickerBirthdayYear);
 
         editTextName.setText(user.getName());
         editTextSurname.setText(user.getSurname());
 
-        DateFormat df = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+        numberPickerBirthdayMonth.setMaxValue(12);
+        numberPickerBirthdayMonth.setMinValue(1);
+        numberPickerBirthdayDay.setMaxValue(31);
+        numberPickerBirthdayDay.setMinValue(1);
+        numberPickerBirthdayYear.setMaxValue(3000);
+        numberPickerBirthdayYear.setMinValue(1500);
+
         Date birthday = user.getBirthday();
-        editTextBirthday.setText(df.format(birthday));
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(birthday);
+        numberPickerBirthdayMonth.setValue(calendar.get(Calendar.MONTH) + 1);
+        numberPickerBirthdayDay.setValue(calendar.get(Calendar.DAY_OF_MONTH));
+        numberPickerBirthdayYear.setValue(calendar.get(Calendar.YEAR));
     }
 
     // Menu save / Items - Save (Create new User or Edit old)
@@ -63,15 +82,20 @@ public class EditActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent = new Intent(this, MainActivity.class);
-        String name = ((EditText) findViewById(R.id.editTextName)).getText().toString();
-        String surname = ((EditText) findViewById(R.id.editTextSurname)).getText().toString();
-        String birthdayStr = ((EditText) findViewById(R.id.editTextBirthday)).getText().toString();
+        String name = editTextName.getText().toString();
+        String surname = editTextSurname.getText().toString();
+
+        StringBuilder birthdayStr = new StringBuilder(Integer.toString(numberPickerBirthdayMonth.getValue()));
+        birthdayStr.append("/");
+        birthdayStr.append(Integer.toString(numberPickerBirthdayDay.getValue()));
+        birthdayStr.append("/");
+        birthdayStr.append(Integer.toString(numberPickerBirthdayYear.getValue()));
 
         Date dateBirthday = new Date();
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
 
         try {
-            dateBirthday = dateFormat.parse(birthdayStr);
+            dateBirthday = dateFormat.parse(birthdayStr.toString());
         } catch (ParseException e) {
             e.printStackTrace();
         }
