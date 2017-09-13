6d3e4919e2ddb3f58ed5f7c68ad0edf95c1d720b
Igor Dr0gis <dr0gis@mail.ru>
2017-09-13 20:20:59 +0300

    Add title to action bar in EditActivity

--- app/src/main/java/com/example/dr0gi/task1users/EditActivity.java
+++ app/src/main/java/com/example/dr0gi/task1users/EditActivity.java
@@ -1,8 +1,10 @@
 package com.example.dr0gi.task1users;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -28,6 +30,10 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        String titleActivity = "Profile info";
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(titleActivity);
 
         Intent intent = getIntent();
         int message = intent.getFlags();
